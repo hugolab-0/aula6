0d8402cdbb261597eb88cdef950c5864d7ebe416
HUGO DE OLIVEIRA ALVES VIEIRA <25203638@CFP127ALUNO.LOCAL>
2026-03-11 10:27:26 -0300

    conhecendo o array

--- array_json.js
+++ array_json.js
@@ -34,7 +34,16 @@
     }
 */
 
-const listaNomes = ['jose', 'maria', 'joao', 10, true]
+const listaNomes = ['arcanjo',
+                    'nicolas',
+                    'nicolas',
+                    'henrique',
+                    'kauan',
+                    'caua', 
+                    'miguel',
+                    'kaiky', 
+                    'rafael',
+                    'pietro']
 const listaCliente    = []
 const listaDeFornecedores= []
 
@@ -42,22 +51,22 @@ const listaDeFornecedores= []
 const exibirDados = function(){
 
     //Exibe o objeto array e seu conteudo
-    console.log (listaNomes)
+    console.table (listaNomes)
 
     //exibe o objeto array em formato de tabela com seus indices
     console.table(listaNomes)
 
     //exibe apenas o valor respectivo indice do array
-    console.log(listaNomes[1])
+    console.table(listaNomes[1])
     
     //retorna um indice de dados do tipo array
-    console.log(typeof(listaNomes[4])
+    console.table(typeof(listaNomes[4])
 
 
     //exibe um nome 
-    // console.log(`O nome do cliente é: ${listaNomes[0]}`)
-    // console.log(`O nome do cliente é: ${listaNomes[1]}`)
-    // console.log(`O nome do cliente é: ${listaNomes[2]}`)
+    // console.table(`O nome do cliente é: ${listaNomes[0]}`)
+    // console.table(`O nome do cliente é: ${listaNomes[1]}`)
+    // console.table(`O nome do cliente é: ${listaNomes[2]}`)
     
 /************
     estruturas de repetição
@@ -65,7 +74,7 @@ const exibirDados = function(){
 
     let cont = 0;
     while(cont <= 4){
-        console.log(`O nome do cliente é: ${listaNomes[3]}`)
+        console.table(`O nome do cliente é: ${listaNomes[3]}`)
         cont++
 
     }
@@ -77,16 +86,16 @@ estruturas de repetição
     for
 
     for(let cont = 0; cont <= 4; cont++){
-        console.log(`O nome do cliente é: ${listaNomes[3]}`)
+        console.table(`O nome do cliente é: ${listaNomes[3]}`)
       cont++
     }
 
 ******************/
 
      //estruturas de repetição
-    //  console.log('---------------FOR EACH----------------')
+    //  console.table('---------------FOR EACH----------------')
     // listaNomes.forEach(function(cliente){
-    //     console.log(`O nome do cliente é: ${cliente}`)
+    //     console.table(`O nome do cliente é: ${cliente}`)
     // })
 
 
@@ -96,7 +105,7 @@ estruturas de repetição
 
 
     for(item in listaNomes){
-        console.log(`o nome do cliente é:  ${listaNomes[item]}`)
+        console.table(`o nome do cliente é:  ${listaNomes[item]}`)
     }
 *************/
 
@@ -118,13 +127,66 @@ const manipularDados = function(){
     listaCliente[1] = 'maria da Silva'
     listaCliente[2] = 'joao martines'
 
-    console.log(listaCliente)
+    console.table(listaCliente)
 
     //o push permite add novos valores no array sempre no final da lista
-    listaDeFornecedores.push('luiz da Silva')
-    listaDeFornecedores.push('Brutal da Silva')
+    listaDeFornecedores.push('Cheshire')
     listaDeFornecedores.push('Fllyn diss')
+    listaDeFornecedores.push('Brutal da Silva')
+    listaDeFornecedores.push('claus da silva')
+    listaDeFornecedores.push('vordx souzas')
 
-    console.log (listaDeFornecedores)
+    console.table (listaDeFornecedores)
+
+    // o unshift sempre add novos valores sempre no inicio da lista
+    listaDeFornecedores.unshift('Lilian cardosa')
+    console.table(listaDeFornecedores)
+
+
+    // Ele remove elementos do final da lista
+    listaDeFornecedores.pop()
+    console.table(listaDeFornecedores)
+
+    // ele remove o conteudo do inicio da lista
+    listaDeFornecedores.shift()
+    console.table(listaDeFornecedores)
+
+    // permite remover um elemento baseado no indice da lista
+            // splice (indice, qtde elementos)
+    listaDeFornecedores.splice(2,1)
+    console.table(listaDeFornecedores)
+
 }
-manipularDados()
+const removerItem = function(nome){
+
+    // retorna o indice de um elemento fazendo a busca pelo valor
+        //  se o indexof nao encontrar o conteudo ele retorna -1
+    let indice = listaNomes.indexOf(nome)
+    if (indice != -1){
+        listaNomes.splice(indice, 1)
+        return true
+    }
+    console.log(indice)
+
+//     for(indice in listaNomes){
+//         if(listaNomes[indice] == nome){
+//             listaNomes.splice(indice,1)
+//         }
+//     }
+}
+const verificarItem = function(nome){
+
+    // include verifica a existencia de um conteudo dentro de uma lista (true/false)
+    return listaNomes.includes(nome)
+}
+const qtdeDados = function(nome){
+    let cont = 0;
+    listaNomes.forEach(function(item){
+        if(String(item).toUpperCase() == String(nome).toUpperCase()){
+            cont++
+        }
+    });
+
+    return cont
+}
+console.log(qtdeDados('nicolas'))
